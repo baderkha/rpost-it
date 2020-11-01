7e4d3fd06c167c634e697ae17befad9dc68829b5
ahmad <ahmad.baderkhan@youvisit.com>
2020-11-01 02:22:20 -0500

    cache repo

--- src/repository/cache_repo.go
+++ src/repository/cache_repo.go
@@ -2,26 +2,19 @@ package repository
 
 import (
 	"github.com/patrickmn/go-cache"
+	"time"
 )
 
 // ICacheRepository : Cache Repository
 type ICacheRepository interface {
+	// uses set expiration
 	Set(key string, value interface{}, expirationTimeSeconds uint) bool
+	// uses default expiration
+	SetDefaultExpiration(key string, value interface{}) bool
+	// sets items with no expiry , use this with caution please
+	SetNoExpiry(key string, value interface{}) bool
+	// gets key or returns not found
 	Get(key string) (item interface{}, isFound bool)
-}
-
-// RedisCacheRepository : caches via redis cache , TODO
-type RedisCacheRepository struct {
-}
-
-// Set  : set in redis the cache required
-func (r *RedisCacheRepository) Set(key string, value interface{}, expirationTimeSeconds uint) bool {
-	return false
-}
-
-// Get : Get item from redis via key
-func (r *RedisCacheRepository) Get(key string) (item interface{}, isFound bool) {
-	return nil, false
 }
 
 // InMemoryCache : Memory Cache using ram
@@ -38,7 +31,25 @@ func (i *InMemoryCache) Get(key string) (item interface{}, isFound bool) {
 func (i *InMemoryCache) Set(key string, value interface{}, expirationTimeSeconds uint) bool {
 	i.Cacher.Set(key,
 		value,
+		time.Duration(expirationTimeSeconds)*time.Second,
+	)
+	return true
+}
+
+// SetDefaultExpiration : sets an item to expire with server enviroment set expiration time
+func (i *InMemoryCache) SetDefaultExpiration(key string, value interface{}) bool {
+	i.Cacher.Set(key,
+		value,
 		cache.DefaultExpiration,
+	)
+	return true
+}
+
+// SetNoExpiry : Adds an item with no expiry
+func (i *InMemoryCache) SetNoExpiry(key string, value interface{}) bool {
+	i.Cacher.Set(key,
+		value,
+		cache.NoExpiration,
 	)
 	return true
 }
